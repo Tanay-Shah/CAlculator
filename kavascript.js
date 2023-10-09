d6fd34f4a65afe907dedf7118447ae072b9c58d4
Tanay-Shah <100477819+Tanay-Shah@users.noreply.github.com>
2023-10-09 20:59:58 +0530

    Update kavascript.js
    fix Bugs

--- kavascript.js
+++ kavascript.js
@@ -1,13 +1,19 @@
 let input = document.getElementById(`inputbox`);
 let buttons = document.querySelectorAll('button');
 let String=``;
-const arr = Array.from(buttons);
 
-arr.forEach(button => {
+buttons.forEach(button => {
     button.addEventListener(`click`,(e) => {
 
+        if(check===1 && e.target.innerHTML != `=`)
+        {
+            String=``;
+            check=0;
+        }
         if(e.target.innerHTML == `=`){
             String=eval(String);
+            ++check;
+            ///eval means evaluate
             input.value=String;
         }
         else if(e.target.innerHTML == `AC`){
